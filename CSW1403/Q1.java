76de7f987cde11b683427100a2d16a7461b40b67
Biswajit Mahapatra <99376983+Biswajit-Mahapatra-02@users.noreply.github.com>
2023-03-14 15:40:41 +0530

    Add files via upload

--- CSW1403/Q1.java
+++ CSW1403/Q1.java
@@ -1,5 +1,7 @@
 package CSW1403;
 
+import java.util.Scanner;
+
 //WAP to take an array as input and find the sum of all the elements of the array
 public class Q1 {
     static int SOA(int[] a){
@@ -11,7 +13,12 @@ public class Q1 {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.print("Enter 5 numbers: ");
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
         System.out.println("The sum of array is: "+SOA(arr));
     }
 }
