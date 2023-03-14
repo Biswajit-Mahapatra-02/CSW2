a60a1fe77a809af9109e169afda4089bea64cdca
Biswajit Mahapatra <99376983+Biswajit-Mahapatra-02@users.noreply.github.com>
2023-03-14 16:17:01 +0530

    Add files via upload

--- CSW1403/Q1.java
+++ CSW1403/Q1.java
@@ -2,23 +2,36 @@ package CSW1403;
 
 import java.util.Scanner;
 
-//WAP to take an array as input and find the sum of all the elements of the array
 public class Q1 {
     static int SOA(int[] a){
         int s = 0;
         for (int x: a) {
-            s+=x;
+            s += x;
         }
         return s;
     }
-
+    static boolean LOA(int[] a, int x){
+        for (int j : a) {
+            if (x == j)
+                return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];
-        System.out.print("Enter 5 numbers: ");
-        for (int i = 0; i < 5; i++) {
+        System.out.print("Enter array length: ");
+        int a = sc.nextInt();
+        System.out.print("Enter "+a+" numbers: ");
+        int[] arr = new int[a];
+        for (int i = 0; i < a; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("The sum of array is: "+SOA(arr));
+        System.out.println("The sum of all the elements of the array: "+SOA(arr) );
+        System.out.print("Enter search element: ");
+        int x = sc.nextInt();
+        System.out.println("Searching...");
+        if(LOA(arr,x)){
+            System.out.println("Element was found.");
+        } else System.out.println("Element does not exist.");
     }
 }
