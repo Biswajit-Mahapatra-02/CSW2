d092abf858026fc08a5c549a2b60e25607ebe3d9
Biswajit Mahapatra <mahapatra.biswajit1102@gmail.com>
2023-03-28 09:26:50 +0530

    Permutation permutated

--- CSW2703/Permutate.java
+++ CSW2703/Permutate.java
@@ -8,13 +8,33 @@ public class Permutate {
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        System.out.println("Enter "+n+" elements: ");
+        System.out.print("Enter "+n+" elements: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        permutate(arr);
+        permutation(arr,0,n);
     }
-    static void permutate(int[] arr){
-        
+    static void permutation(int[] arr, int i, int length){
+        if (length == i) {
+            printArray(arr, length);
+            return;
+        }
+        int j;
+        for (j = i; j < length; j++) {
+            swap(arr, i, j);
+            permutation(arr, i + 1, length);
+            swap(arr, i, j);
+        }
+    }
+    static void printArray(int[] arr, int length){
+        for (int i = 0; i < length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    static void swap(int[] arr, int x, int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
