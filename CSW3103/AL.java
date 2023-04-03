5267e9deee7686d587cf6c8a0c4f45f128750370
Biswajit Mahapatra <mahapatra.biswajit1102@gmail.com>
2023-04-03 17:08:13 +0530

    Correction

--- CSW3103/AL.java
+++ CSW3103/AL.java
@@ -5,21 +5,21 @@ package CSW3103;
 import java.util.*;
 public class AL {
     public static void main(String[] args) {
-        ArrayList<Integer> num = new ArrayList<>();
-        num.add(1);
-        num.add(2);
-        num.add(3);
-        num.add(4);
-        num.add(5);
-        num.add(6);
-        num.add(7);
+        ArrayList<String> num = new ArrayList<>();
+        num.add("1");
+        num.add("2");
+        num.add("3");
+        num.add("4");
+        num.add("5");
+        num.add("6");
+        num.add("7");
         System.out.println("Current arraylist: \t\t"+num);
         System.out.println("The reversed array list: "+reverseArrayList(num));
         System.out.println("Maximum element in the list: "+Collections.max(num));
         System.out.println("Minimum element in the list: "+Collections.min(num));
 
         System.out.print("List iterator reverse: ");
-        ListIterator<Integer> rev = num.listIterator(num.size());
+        ListIterator<String> rev = num.listIterator(num.size());
 
         while(rev.hasPrevious()){
             System.out.print(rev.previous()+" ");
@@ -29,8 +29,8 @@ public class AL {
         Collections.swap(num, 2, 6);
         System.out.println("Arraylist after swapping: "+num);
     }
-    static ArrayList<Integer> reverseArrayList(ArrayList<Integer> AL){
-        ArrayList<Integer> temp = new ArrayList<>();
+    static ArrayList<String> reverseArrayList(ArrayList<String> AL){
+        ArrayList<String> temp = new ArrayList<>();
         for (int i = AL.size() - 1; i >= 0; i--) {
             temp.add(AL.get(i));
         }
